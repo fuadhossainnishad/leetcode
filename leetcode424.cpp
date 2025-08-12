22acf93b5b84f246ddcd7c8256f4a113f1b5a3b0
Fuad Hossain <fuadhossainbk01@gmail.com>
2025-08-12 00:20:43 +0000

    initiate leetcode 424 problem

---
+++ leetcode424.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+    
+int characterReplacement(string s, int k) {
+        
+    }
+
+int main() {
+    string s="ABAB";
+    int k=2;
+    int res = characterReplacement(s,k);
+    cout << res;
+    return 0;
+}
